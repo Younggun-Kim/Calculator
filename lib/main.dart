0930fe8b217617dfae14b11f256f5b94ae772499
Conner <dudrjs145@naver.com>
2025-01-10 21:04:58 +0900

    Init Project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text('컨두잇 코딩테스트'),
+          ),
+        ),
+      ),
+    );
+  }
+}
